a645c946b1dd83efca257461fa22ea801d33649d
Timothee Goguely <timotheegoguely@gmail.com>
2017-02-13 18:21:34 +0100

    remove first_name, last_name and gener validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   # Validations
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :gender, presence: true, inclusion: { in: %w(male female), allow_nil: false, message: "%{value} is not a valid gender" }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, on: :create
 end
