051887dd4e12ecb28729b6bdd6acf95cf2055332
Akram Dhane <akram.dhane@gmail.com>
2017-02-13 17:25:22 +0100

    update DB schema + add Review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20170213152816) do
 
-ActiveRecord::Schema.define(version: 20170213145759) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 20170213145759) do
     t.datetime "updated_at",                       null: false
     t.string   "manufacturer"
     t.index ["user_id"], name: "index_hubots_on_user_id", using: :btree
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "rating"
+    t.boolean  "is_from_owner"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "users", force: :cascade do |t|
