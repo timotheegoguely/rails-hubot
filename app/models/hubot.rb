e9ade8455796fe9ff5b2fc5db212f560f05d5430
Timothee Goguely <timotheegoguely@gmail.com>
2017-02-13 15:42:06 +0100

    Add default value for Hubots availability and update Hubot validations

--- app/models/hubot.rb
+++ app/models/hubot.rb
@@ -6,15 +6,15 @@ class Hubot < ApplicationRecord
   validates :name, presence: true, allow_blank: false
   validates :type, presence: true, allow_blank: false
   validates :model, presence: true, allow_blank: false
-  validates :age, numericality: { only_integer: true }
-  validates :turing_test_score, numericality: { only_integer: true }
+  validates :age, numericality: { only_integer: true }, length: { in: 10..80 }
+  validates :turing_test_score, numericality: { only_integer: true }, length: { in: 1..100 }
   validates :desposit, numericality: { only_integer: true }
   validates :price, numericality: { only_integer: true }
   validates :weight, numericality: true
   validates :height, numericality: true
   validates :autonomy, numericality: true
   validates :charging_time, numericality: true
-  validates :availability, default: true
-  validates :gender, inclusion: { in: ["male","female"], allow_nil: false }
-  validates :skin_color, inclusion: { in: ["white","asian","brown","black"], allow_nil: false }
+  validates :availability
+  validates :gender, inclusion: { in: ["male","female"], message:"%{value} is not a valid gender", allow_nil: false }
+  validates :skin_color, inclusion: { in: ["white","asian","brown","black"], message:"%{value} is not a valid skin color" allow_nil: false }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213135506) do
+ActiveRecord::Schema.define(version: 20170213143720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,13 +32,13 @@ ActiveRecord::Schema.define(version: 20170213135506) do
     t.string   "accessories"
     t.string   "composition"
     t.string   "maintenance"
-    t.boolean  "availability"
+    t.boolean  "availability",      default: true
     t.string   "skin_color"
     t.integer  "deposit"
     t.integer  "price"
     t.integer  "user_id"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.index ["user_id"], name: "index_hubots_on_user_id", using: :btree
   end
 
