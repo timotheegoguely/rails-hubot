815cde57963490668701828751efed0d5634c450
Timothee Goguely <timotheegoguely@gmail.com>
2017-02-13 15:19:09 +0100

    Add validations to Hubot model

--- app/models/hubot.rb
+++ app/models/hubot.rb
@@ -1,3 +1,20 @@
 class Hubot < ApplicationRecord
+  # Association
   belongs_to :user
+
+  # Validations
+  validates :name, presence: true, allow_blank: false
+  validates :type, presence: true, allow_blank: false
+  validates :model, presence: true, allow_blank: false
+  validates :age, numericality: { only_integer: true }
+  validates :turing_test_score, numericality: { only_integer: true }
+  validates :desposit, numericality: { only_integer: true }
+  validates :price, numericality: { only_integer: true }
+  validates :weight, numericality: true
+  validates :height, numericality: true
+  validates :autonomy, numericality: true
+  validates :charging_time, numericality: true
+  validates :availability, default: true
+  validates :gender, inclusion: { in: ["male","female"], allow_nil: false }
+  validates :skin_color, inclusion: { in: ["white","asian","brown","black"], allow_nil: false }
 end
