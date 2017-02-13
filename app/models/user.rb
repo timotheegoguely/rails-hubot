ec4193de7a193cf117b27ae5fcad1f40212ed5f0
Akram Dhane <akram.dhane@gmail.com>
2017-02-13 15:36:11 +0100

    change validations & default values on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Validations
+  validates :first_name, presence: true, uniqueness: { case_sensitive: false }
+  validates :last_name, presence: true, uniqueness: { case_sensitive: false, scope: :first_name, message: "This user already exists" }
+  validates :gender, presence: true, inclusion: { in: %w(male female), message: "%{value} is not a valid gender" }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, on: :create
 end
