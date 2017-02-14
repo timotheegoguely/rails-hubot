ba09060c01142013535b7bc7c935d56fa0e5fcab
Akram Dhane <akram.dhane@gmail.com>
2017-02-14 12:17:49 +0100

    update users routes & controllers considering devise gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,7 @@
 class UsersController < ApplicationController
 
   skip_before_action :authenticate_user!, only: :show
-  before_action :set_user, only: [ :show, :edit, :update, :hubots ]
-
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
-  # POST /users
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to user_path(@user)
-    else
-      render :new
-    end
-  end
-
-  # GET /users/:id/edit
-  def edit
-  end
-
-  # PATCH /users/:id
-  def update
-    @user.update(user_params)
-    redirect_to user_path(@user)
-  end
+  before_action :set_user, only: [ :show, :hubots ]
 
   # GET /users/:id
   def show
@@ -40,10 +15,6 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
-  end
-
-  def user_params
-    params.require(:user).permit(:email)
   end
 
 
